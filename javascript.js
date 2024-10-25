c911d7f1f50414fbd37915c5a84a080f71e0cafc
parkervankley <parkerjvankley@gmail.com>
2024-10-24 21:50:38 -0500

    Create static calculator

---
+++ javascript.js
@@ -0,0 +1,13 @@
+function operate(a, b, operator) {
+  switch (operator) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+  }
+}
+
